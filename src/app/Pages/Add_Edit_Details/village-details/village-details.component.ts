3a79705e1a443b70c41e91b918edcfe8766ec141
ShalineeMishra <nkbpl.shalinee@gmail.com>
2022-01-24 19:18:31 +0530

    Added some more APIs in Village details page

--- src/app/Pages/Add_Edit_Details/village-details/village-details.component.ts
+++ src/app/Pages/Add_Edit_Details/village-details/village-details.component.ts
@@ -1,14 +1,13 @@
 import { Component,AfterViewInit, OnInit,ViewChild } from '@angular/core';
-import {  NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpResponse,HttpClientModule,HttpHeaders } from '@angular/common/http';
-import { DataTableDirective } from 'angular-datatables';
-import { Subject } from 'rxjs';
+import { UtilityService } from 'src/app/services/utility.service';
 import { Router } from '@angular/router';
 import { UrlService } from 'src/app/services/url.service';
 import { APIUtilityService } from 'src/app/services/APIUtility.service';
 import { StateDetails,DistrictDetails,TalukaDetails,VillageDetails,SearchCriteria, DropDownChangeEnum, FilterControls} from 'src/app/Model/Filters.model';
-import { VillageResponseModel,VillageChainageModel } from 'src/app/Model/Village.model';
+import { VillageResponseModel,VillageChainageModel,AddOrUpdateVillageChainageResponseModel } from 'src/app/Model/Village.model';
 import {HttpService} from '../../../services/http.service';
+import { Subject, from } from 'rxjs';
 
 @Component({
   selector: 'app-village-details',
@@ -17,38 +16,42 @@ import {HttpService} from '../../../services/http.service';
 })
 
 export class VillageDetailsComponent implements OnInit {
-  /**datatable proprties */
-  @ViewChild(DataTableDirective, {static: false})
-  dtElement: DataTableDirective;
-  dtTrigger: Subject<any> = new Subject();
-  dtOptions: DataTables.Settings = {};
-  datatable: any;
-  
   _StateDataModel : StateDetails[];
   _DistrictDetails : DistrictDetails[];
   _TalukaDetails : TalukaDetails[];
   _VillageDetails : VillageDetails[];
   _SearchCriteria : SearchCriteria;
   _VillageResponseModel : VillageResponseModel;
+  _VillageChainageResModel : AddOrUpdateVillageChainageResponseModel;
   /**onchange of taluka dropdown village details form show/hide */
   _ShowVillageDetailsDiv : boolean = false;
+
+  /**enable/disable input fields variables*/
   _DisabledInputField : boolean = true;
-  _AddNewVillage : boolean = false;
+  _DisabledChainageField : boolean = true;
+
+  _NewVillageAdd : boolean = false;
 
   _FilterControls : FilterControls;
+  _AdditingMode : boolean = false;
+ 
 
   constructor(
     public urlService: UrlService,
     public APIUtilityService: APIUtilityService,
     private router: Router,
     private httpService: HttpService,
+    public Utility :UtilityService
     ){ 
       this._SearchCriteria = new SearchCriteria();
       this._VillageResponseModel = new VillageResponseModel();
+      this._VillageChainageResModel = new AddOrUpdateVillageChainageResponseModel();
       this._FilterControls = new FilterControls();
       this.SetFilterControls();
+
     }
 
+  /**hide/show filter menu based on the component requirement */
   SetFilterControls()
   {
     this._FilterControls.ShowState = true;
@@ -59,92 +62,29 @@ export class VillageDetailsComponent implements OnInit {
   
   ngOnInit(): void 
     {
-      this.dtOptions = 
-        {
-          pagingType: 'full_numbers',
-          pageLength: 10,
-        };
-      /**populated all states name on page*/
-      this.PopulateState();
+      
     }
 
-  ngAfterViewInit(): void 
+  
+
+  /**1. Get Values From Filters component and assign into SearchCriteria
+  * 2.  Call AddOrUpdateVillage API
+  */
+  GetValuesFromFilters(event)
   {
-    this.dtTrigger.next();
-  }
-
-  ngOnDestroy(): void 
-  {
-    // Do not forget to unsubscribe the event
-    this.dtTrigger.unsubscribe();
-  }
-
-  /**refresh datatable  */
-  rerenderDataTable(): void {
-    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-      // Destroy the table first
-      dtInstance.destroy();
-      // Call the dtTrigger to rerender again
-      this.dtTrigger.next();
-    });
-  }
-
-  /**API CALL FOR state details */
-  PopulateState()
-    {
-      let url = this.urlService.GetAllStatesAPI; 
-      this.httpService.get(url,null).subscribe(res => {
-        this._StateDataModel = res;
-        },error => {
-          console.log("error",error);
-        });
+    this.Utility.LogText(event);
+    this._SearchCriteria = event;
+    if (Object.keys(this._SearchCriteria).length === 0) {
+      alert("Please Select State,District,taluka or village!!")
     }
-
-  /**Get all District list base on the selected state */
-  PopulateDistrict(argStateId)
-  {
-    this.ResetDropDowns(DropDownChangeEnum.StateChanged);
-    let url = this.urlService.GetDistrictByStateAPI + argStateId; 
-    this.httpService.get(url,null).subscribe(response => {
-      this._DistrictDetails = response;
-      },error => {
-        console.log("error",error);
-      });
-  }
-
-  /**get all Taluka details base on the selected DistrictId */
-  PopulateTaluka(argDistrictID)
-    {
-      this.ResetDropDowns(DropDownChangeEnum.DistrictChanged)
-      let url = this.urlService.GetTalukaByDistrictAPI+ argDistrictID;
-      this.httpService.get(url,null).subscribe(response => {
-        this._TalukaDetails = response;
-        },error => {
-          console.log("GetTalukaByDistrictAPI error",error);
-        });
-    }
-
-    /**get all village details base on the selected Taluka */
-    GetVillageByTalukaId(argTalukaId)
-    {
-      this.ResetDropDowns(DropDownChangeEnum.TalukaChanged);
-      let url = this.urlService.GetVillageByTalukaAPI + argTalukaId;
-      this.httpService.get(url,null).subscribe(response => {
-        this._VillageDetails = response;
-        },error => {
-          console.log("GetVillageByTalukaAPI error",error);
-        });
-    }
-
-    /**search village details based on the selected dropdowns 
-     * and call AddOrUpdateVillage API
-    */
-    SearchData(){
-      console.log('SearchData',this._SearchCriteria)
+    else{
+      /**show village details html div */
+      this._ShowVillageDetailsDiv = true;
       this.GetVillageByVillageId();
-
     }
+  }
 
+  /**get village details based on the selected filter values */
   GetVillageByVillageId()
     {
       let url = this.urlService.GetVillageByVillageIdAPI + this._SearchCriteria.VillageId;
@@ -152,61 +92,78 @@ export class VillageDetailsComponent implements OnInit {
         this._VillageResponseModel.Result = response;
         console.log('GetVillageByVillageIdAPI response', this._VillageResponseModel);
         /**only for test */
-        this._VillageResponseModel.Result.Chainages = [
-          {
-            "VillageId": 0,
-            "VillageChainageId": 0,
-            "ChainageFrom": 0,
-            "ChainageTo": 0,
-            "SurveyAgency": "test",
-            "LengthInKm": 0
-          }
-        ];
-        /**show village details html div */
-        this._ShowVillageDetailsDiv = true;
+        // this._VillageResponseModel.Result.Chainages = [
+        //   {
+        //     "VillageId": 0,
+        //     "VillageChainageId": 1,
+        //     "ChainageFrom": 0,
+        //     "ChainageTo": 0,
+        //     "SurveyAgency": "test",
+        //     "LengthInKm": 0
+        //   }
+        // ];
         },error => {
-          console.log("GetVillageByVillageIdAPI error",error);
+          console.log("Get Village By VillageId API error",error);
         });
     }
 
 
 
-  /**add NEW village details in data base
-  * show process button 
-  */
+    /**add NEW village details in database 
+    *  show Add details button 
+    */
   AddVillageDetails()
-   {
-    this._AddNewVillage = true
-    this._DisabledInputField = false
-   }
-
-  /**
-   *enable input fields for village information edit
-  */
-  EditVillageDetails()
     {
       this._DisabledInputField = false;
+      this._NewVillageAdd = true;
+      // this._DisabledChainageField = false;
+    }
+
+  /**
+  *enable input fields for village information edit
+  */
+  EditingModeOntoVillageDetails()
+    {
+      this._DisabledInputField = false;
+      this._DisabledChainageField = false;
+      this._NewVillageAdd = false;
+
     }
 
 
-  /**when add new village or updated village information call
-  * AddOrUpdateVillage API 
-  * At the time of editing chainage save separately
-  */
-  AddOrUpdateVillageDetails(){
-    let url = this.urlService.AddOrUpdateVillageAPI;
-    this.httpService.Post(url,this._VillageResponseModel.Result).subscribe(response => {
-      this._VillageResponseModel = response;
-      console.log('AddOrUpdateVillageAPI response', this._VillageResponseModel);
-      },error => {
-        console.log("AddOrUpdateVillageAPI error",error);
-      });
-  }
-
-  AddChainageDetails(){
-      let data : VillageChainageModel;
+  /**add more items in the chainage array*/
+  AddChainageItem()
+    {
+      this._DisabledChainageField = false;
+      let data = new VillageChainageModel();
       this._VillageResponseModel.Result.Chainages.push(data)
-  }
+    }
+
+  /**delete Chainage Details item from chainage array*/
+  DeleteChainageItem(index)
+    {
+      this._VillageResponseModel.Result.Chainages.splice(index, 1);  
+    }
+
+
+  /**1. when add new village or updated village information call AddOrUpdateVillage API
+    *2. At the time of editing chainage details save separately
+  */
+  SaveVillageDetails()
+    {
+      this._VillageResponseModel.Result.Chainages.push(this._VillageChainageResModel.Result);
+      let url = this.urlService.AddOrUpdateVillageAPI;
+      this._VillageResponseModel.Result.VillageId = this._SearchCriteria.VillageId;
+      this._VillageResponseModel.Result.TalukaId = this._SearchCriteria.TalukaId;
+      this.httpService.Post(url,this._VillageResponseModel.Result).subscribe(response => {
+        this._VillageResponseModel = response;
+        console.log('AddOrUpdateVillageAPI response', this._VillageResponseModel);
+        alert("Village added sucessfully!!")
+        },error => {
+          console.log("AddOrUpdateVillageAPI error",error);
+        });
+    }
+
 
   /**delete village details base on the selected villageID */
   DeleteVillageDetails()
@@ -219,62 +176,60 @@ export class VillageDetailsComponent implements OnInit {
           console.log("AddOrUpdateVillageAPI error",error);
         });
     }
+   
+  /**chainage details edit separate */
+  EditOnlyChaingeDetails()
+  {
+    this._DisabledInputField = false;
+    this._DisabledChainageField = true;
+  }
 
-    /**save VillageDetails */
-    SaveVillageDetails(){
-      console.log("add data",this._VillageResponseModel.Result)
+ 
+  /**AT the time of editing chainage details save separate API */
+  SaveChaingeDetails()
+    {
+      this._VillageResponseModel.Result.Chainages.forEach(element => {
+        element.VillageId = this._SearchCriteria.VillageId;
+      });
+      let url = this.urlService.AddOrUpdateVillageChainageAPI;
+      this.httpService.Post(url,this._VillageResponseModel.Result.Chainages).subscribe(response => {
+        this._VillageChainageResModel = response;
+        alert(this._VillageChainageResModel.Message);
+        console.log('AddOrUpdateVillageChainageAPI response', this._VillageChainageResModel);
+        /**added newly add chainge to the villaage details 
+         * Refresh datatable 
+        */
+        this._VillageResponseModel.Result.Chainages.push(this._VillageChainageResModel.Result);
+        },error => {
+          console.log("AddOrUpdateVillageChainageAPI error",error);
+        });
     }
 
-    /**
-     * This fucntion empty the colelction which binds the dropdown for state/district/tehsil/vilage respectivly based onthe
-     * argument.
-     * @param argSelection This defines what dropdown has changed. Based on this value below dropdowns will be changed
-    */
-    ResetDropDowns(argSelection : DropDownChangeEnum)
-     {
-       switch(argSelection)
-       {
-         case DropDownChangeEnum.StateChanged: 
-           this._DistrictDetails = [];
-           this._TalukaDetails = [];
-           this._VillageDetails = [];
-           this._ShowVillageDetailsDiv = false;
-           this._DisabledInputField = true;
-          //  this._VillageResponseModel = null;
-           this.ResetSelectedValue(argSelection);
-           break;
-         case DropDownChangeEnum.DistrictChanged: 
-           this._TalukaDetails = [];
-           this._VillageDetails = [];
-           this._ShowVillageDetailsDiv = false;
-           this._DisabledInputField = true;
-          //  this._VillageResponseModel = null;
-           this.ResetSelectedValue(argSelection);
-           break;
-         case DropDownChangeEnum.TalukaChanged: 
-           this._VillageDetails = [];
-          //  this._VillageResponseModel = null;
-           this.ResetSelectedValue(argSelection);
-           break;
-       }
-     }
- 
-     ResetSelectedValue(argSelection : DropDownChangeEnum)
-       {
-         switch(argSelection)
-         {
-             case DropDownChangeEnum.StateChanged:
-               this._SearchCriteria.DistrictId = null;
-               this._SearchCriteria.TalukaId = null;
-               this._SearchCriteria.VillageId = null;
-               break;
-             case DropDownChangeEnum.DistrictChanged:
-               this._SearchCriteria.TalukaId = null;
-               this._SearchCriteria.VillageId = null;
-               break;
-             case DropDownChangeEnum.TalukaChanged:
-               this._SearchCriteria.VillageId = null;
-               break;
-         }
-       }
+      /**AT the time of editing chainage details save separate API */
+  DeleteChaingeDetails(argVillageChainageId)
+    {
+      let url = this.urlService.DeleteVillageChainageAPI + argVillageChainageId;
+      this.httpService.Post(url,null).subscribe(response => {
+        let resp : AddOrUpdateVillageChainageResponseModel = response;
+        alert(resp.Message);
+        },error => {
+          console.log("AddOrUpdateVillageAPI error",error);
+        });
+    }
+
+
+  // GetChainageTableData() {
+  //   let headers: string[] = [];
+  //   if(this._VillageResponseModel.Result.Chainages) {
+  //     this._VillageResponseModel.Result.Chainages.forEach((value) => 
+  //     {
+  //       Object.keys(value).forEach((key) => {
+  //         if(!headers.find((header) => header == key)){
+  //           headers.push(key)
+  //         }
+  //       })
+  //     })
+  //   }
+  //   return headers;
+  // }
 }
